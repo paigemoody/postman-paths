30ea783b8f5c782a84f0339f16db0b6f21b1eda2
paigemoody <moodypaige.e@gmail.com>
2019-05-17 23:37:00 +0000

    move dataframe constructor to edges lengths function, start moving functions out of jupyter

---
+++ circuit_constructor.py
@@ -0,0 +1,53 @@
+import numpy as np
+import osmnx as ox
+import networkx as nx
+import itertools
+from random import choice
+import math
+
+
+# BUILD GRAPH & GET STATS
+def build_graph(north,south, east, west):
+
+    """
+    From boox constraints, construct graph
+
+    Print summary stats about graph.
+    """
+    # set bbox bounds: (will eventually come from drawing)
+     
+    # create graph
+    G = ox.graph_from_bbox(north, south, east, west, network_type='walk')
+
+    # display graph
+    # ox.plot_graph(G, node_size=60)
+
+    # Get graph info 
+    basic_stats = ox.basic_stats(G)
+        
+    print("\n n (number of nodes in the graph):", basic_stats['n'])
+    print("\n m (number of edges in the graph):", basic_stats['m']/2) # for undirected graph
+    print("\n street_length_total (sum in meters):", basic_stats['street_length_total']/2)
+
+    return G
+
+
+def make_euler_circuit(start_node, graph):
+    print(start_node, graph.nodes)
+
+if __name__ == '__main__':
+
+    # default bbox
+    NORTH = 37.7599 # max lat 
+    SOUTH = 37.7569 # min lat
+    EAST = -122.3997 # max lng
+    WEST = -122.4023 # min lng 
+
+        
+    G = build_graph(NORTH,SOUTH, EAST, WEST) ## FLAG
+
+    start_node = choice(list(G.nodes))
+
+    make_euler_circuit(start_node, G)
+
+
