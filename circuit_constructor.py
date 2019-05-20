4406b73b1c0769ba8705ab1da2066185ca06a5a7
paigemoody <moodypaige.e@gmail.com>
2019-05-20 22:55:36 +0000

    remove old euler circuit function

--- circuit_constructor.py
+++ circuit_constructor.py
@@ -177,6 +177,7 @@ def make_euler_circuit(start_node, updated_graph_instance):
     current_node = start_node 
 
     node_visit_order = [current_node]
+    edge_visit_order = []
 
 
     while len(current_edges_on_graph_list) > 0:
@@ -206,135 +207,21 @@ def make_euler_circuit(start_node, updated_graph_instance):
         edge_to_traverse_list.remove(current_node)
         # update current node to be the only node left in the edge list
         
+        # update edge traveral list with edge just traversed
+        edge_traversed = (current_node, edge_to_traverse_list[0])
+
+        edge_visit_order.append(edge_traversed)
+
         current_node = edge_to_traverse_list[0]
         
         # add the new current node to the nodes visit order list 
         node_visit_order.append(current_node)
         
-    return node_visit_order
+    return {
+            "node_visit_order": node_visit_order, 
+            "edge_visit_order": edge_visit_order
+            }
 
-# def make_euler_circuit(start_node, graph):
-    
-    
-    # # print("Start node:", start_node)
-    
-    # # SET UP EULER GRAPH
-    # # get all odd nodes on graph 
-    # odd_nodes = get_odd_nodes(graph.nodes_dict)
-    
-    # print("\nodd nodes:", odd_nodes)
-    
-    # # get all possible pairings for odd nodes in graph
-    # # nodes_dict = make_nodes_dict(G)
-    
-    # # print("\nnodes dict:", nodes_dict)
-
-    # print("\nnodes dict:", graph.nodes_dict)
-    
-    # # STARTS GETTING REAL SLOW
-    
-    # # get list of all possible pairings of odd nodes:
-    # possible_pairings_list = get_list_of_all_pairs_lists(odd_nodes)
-    
-    # print("\npossible_pairings_list:", possible_pairings_list)
-    
-    # # get the length of each possible pairing of odd nodes 
-    # length_pairings_lists_dict = get_dict_length_pairings_lists(possible_pairings_list, graph)
-    
-    # print("\nlength pairings lists dict:", length_pairings_lists_dict)
-    
-    # # get the odd nodes pairing that has the shortest distance 
-    # optimal_pairing_list_dict = get_optimal_pairing_list_dict(length_pairings_lists_dict)
-    
-    # print("\noptimal_pairing_list_dict:", optimal_pairing_list_dict)
-    
-    # # get list of edges that will need to be traversed twice 
-    # twice_traversal_list = get_all_double_back_edges(optimal_pairing_list_dict, graph)
-    
-    # print("\ntwice_traversal_list:", twice_traversal_list)
-    
-    # # get dict of edges and how many times each needs to be traversed 
-    # edge_traversals_dict_eulerian = make_traversal_dict_with_added_edges(twice_traversal_list, graph) 
-    
-    # print("\nedge_traversals_dict_eulerian:", edge_traversals_dict_eulerian)
-    
-    # # get list of all edges that need to be traversed, multiple traversal edges are included multiple times
-    # all_edges_in_euler_graph = get_list_of_edges_by_traversal_count(edge_traversals_dict_eulerian)
-
-    # print("\nall_edges_in_euler_graph:", all_edges_in_euler_graph)
-    
-    # # ---- # 
-    
-    # print("\n\n\n\n\n\n\n\nBUILDING CIRCUIT")
-    
-    # # GET EULER CIRCUIT 
-    
-    # # make list of edges currently on graph: 
-    # current_edges_on_graph_list = all_edges_in_euler_graph
-    
-    # # initialize current node 
-    # current_node = start_node 
-    
-    # # initialize a list the contains the order of node visists with the start node
-    
-    # node_visit_order = [current_node]
-    
-    # print("--------START-WALKING--------")
-    
-    # while len(current_edges_on_graph_list) > 0:
-        
-        # print("Node visit order:", node_visit_order)
-        
-        # print("\n\nCURRENT NODE:", current_node)
-        
-        # print edges currently on the graph
-        # print("\nedges currently on graph:", current_edges_on_graph_list )
-
-        # get the bridges on the initial graph
-        # current_bridges_on_graph = get_bridges(current_edges_on_graph_list)
-        # print("\nbridges currently on graph:", current_bridges_on_graph)
-
-        # get all the connected edges in initial graph for the current node 
-        # edges_conn_to_current_node = get_all_conn_edges_remaining_in_graph(current_node, 
-        #                                                                    current_edges_on_graph_list, 
-        #                                                                    nodes_dict)
-        # print(f"\ncurrent node: {current_node} is still connected to: {edges_conn_to_current_node}")
-
-        # get the next edge to traverse 
-        # edge_to_traverse = choose_edge_to_traverse(current_node, edges_conn_to_current_node, current_bridges_on_graph)
-        # print(f"\nedge_to_traverse:", edge_to_traverse)
-
-        # remove edge just traversed from current_edges_on_graph_list
-        ## you don't know the order of the nodes in the edge in the current_edges_on_graph_list,
-        ## so you have to check both
-        
-        # print("\nlen of current_edges_on_graph_list BEFORE removing one item:", len(current_edges_on_graph_list))
-        
-        # if edge_to_traverse in current_edges_on_graph_list:
-        #     current_edges_on_graph_list.remove(edge_to_traverse)
-        # else:
-        #     current_edges_on_graph_list.remove(edge_to_traverse[::-1])
-        
-        # print("\nlen of current_edges_on_graph_list AFTER removing one item:", len(current_edges_on_graph_list))
-        
-        # update current node to be node in edge_to_traverse that is not the current node
-        ## make edge a list so that you can remove the current node
-        
-        # print("\ncurrent node BEFORE update:", current_node)
-        # edge_to_traverse_list = list(edge_to_traverse)
-        ## remove current node from edge to traverse
-        # edge_to_traverse_list.remove(current_node)
-        ## update current node to be the only node left in the edge list
-        
-        # current_node = edge_to_traverse_list[0]
-        # print("\ncurrent node AFTER update:", current_node)
-        
-        # add the new current node to the nodes visit order list 
-        # node_visit_order.append(current_node)
-        
-        # print("\n\n\n----------WALKING-----------")
-
-    # return(node_visit_order)
 
 if __name__ == '__main__':
 
@@ -361,7 +248,10 @@ if __name__ == '__main__':
 
     # # 3. calculate euler circuit 
     print("\n\nEuler circuit node order:")
-    print(make_euler_circuit(start_node, updated_graph_inst))
+    print("node_visit_order:", make_euler_circuit(start_node, updated_graph_inst)["node_visit_order"])
+    print("edge_visit_order:", make_euler_circuit(start_node, updated_graph_inst)["edge_visit_order"])
+
+
 
     print("\n\nProcess time:", time.time() - start, "seconds")
 
