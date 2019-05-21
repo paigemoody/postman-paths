ce1ab4ed982a3bf1c4b1e905383e08ea139b4935
paigemoody <moodypaige.e@gmail.com>
2019-05-21 15:58:22 -0700

    add complete route server route, load complete route on map load (to test functionality)

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from jinja2 import StrictUndefined
 
-from flask import (Flask, render_template, redirect, request, flash, session)
+from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+
+import json
 
 app = Flask(__name__)
 
@@ -20,11 +22,20 @@ def mapview():
 
     return render_template("mapview.html")
 
+
+@app.route('/route_geometry.geojson')
+def route_geometry():
+    """Route geometry."""
+
+    with open('static/example_complete_route.geojson') as json_file:  
+        data = json.load(json_file)
+
+    return jsonify(data)
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
     app.debug = True
-    
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
 
