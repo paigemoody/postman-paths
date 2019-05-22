c68316e2d421d284a30a604e7a18fca3dd20b19e
paigemoody <moodypaige.e@gmail.com>
2019-05-21 22:19:25 -0700

    add geolocate - doesn't work in chrome

--- server.py
+++ server.py
@@ -35,13 +35,13 @@ def route_geometry():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
     # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
+    # app.jinja_env.auto_reload = app.debug
 
     # connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
